93a209cc5da6bac336d80d9bbba1d8f44a90113c
Angel-ISO <angelgabrielorteg@gmail.com>
2026-03-09 23:44:11 -0500

    chore(cors): update CORS allowed origins

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,7 @@
 import tailwindcssAnimate from "tailwindcss-animate";
 import tailwindcssAspectRatio from "@tailwindcss/aspect-ratio";
 
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
